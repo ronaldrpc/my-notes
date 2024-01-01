9f94814a9874135f273b8be0322e2f9711dd3e75
Ronald Paternina <ronaldpaternina11@gmail.com>
2024-01-01 17:53:30 -0500

    feat(Note): add Note app with model and register in admin

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField("Description")
+    create_date = models.DateTimeField(auto_now_add=True)
+    last_modified_date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
