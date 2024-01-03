1686aa337f9c294394ceb91798b89919916de216
Ronald Paternina <ronaldpaternina11@gmail.com>
2024-01-03 12:08:51 -0500

    feat(Notes): add templates and views to Notes app

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'notes'
 urlpatterns = [
     path('', views.home, name="index"),
+    path('<int:note_id>/', views.detail, name="detail"),
 ]
 
--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Note
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Aqui van a ir mis notas e.e")
+    notes = Note.objects.all().order_by('-last_modified_date')
+    context = {'notes': notes}
+    return render(request, 'notes/index.html', context)
 
+def detail(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    return render(request, 'notes/detail.html', {'note': note})
