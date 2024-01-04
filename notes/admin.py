ab9e8bc88fde2c8bc0c415257f3197bf13ac1484
Ronald Paternina <ronaldpaternina11@gmail.com>
2024-01-04 13:13:49 -0500

    feat(Notes,mynotes): add custom template admin and modify admin panel

--- notes/admin.py
+++ notes/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Note
 
-# Register your models here.
 
-admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    fields = ['title', 'description']
+    list_display = ['title', 'description', 'create_date', 'last_modified_date']
+    list_filter = ['last_modified_date']
+    search_fields = ['title']
+
+admin.site.register(Note, NoteAdmin)
