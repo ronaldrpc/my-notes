2e9639d19aedbcbdb90729ca2a6b3813468d6da8
Ronald Paternina <ronaldpaternina11@gmail.com>
2024-01-11 13:52:37 -0500

    Change urls path in settings and notes. Improving base template and title in other ones.

--- mynotes/urls.py
+++ mynotes/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('notes/', include('notes.urls'))
+    path('', include('notes.urls')),
 ]
--- notes/urls.py
+++ notes/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 app_name = 'notes'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
-    re_path('(?:(?P<status>[a-zA-Z]+))?/$', views.IndexView.as_view(), name="index_archived"),  # Optional kwarg
-    path('<int:pk>/', views.DetailView.as_view(), name="detail"),
-    path('<int:note_id>/update/', views.update, name="update"),
-    path('create/', views.create, name="create"),
+    path('notes/', views.IndexView.as_view(), name="index"),
+    re_path('notes/(?:(?P<status>[a-zA-Z]+))?/$', views.IndexView.as_view(), name="index_archived"),  # Optional kwarg
+    path('notes/<int:pk>/', views.DetailView.as_view(), name="detail"),
+    path('notes/<int:note_id>/update/', views.update, name="update"),
+    path('notes/create/', views.create, name="create"),
     path('note/create/', views.NoteCreateView.as_view(), name="note-create"),
     path('note/<int:pk>/', views.NoteUpdateView.as_view(), name="note-update"),
 ]
