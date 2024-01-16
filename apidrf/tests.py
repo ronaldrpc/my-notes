f5e2128adc6f33ff86775c218c53d82d054320cc
Ronald Paternina <ronaldpaternina11@gmail.com>
2024-01-16 01:47:04 -0500

    test(apidrf): add put and delete note tests.

--- apidrf/tests.py
+++ apidrf/tests.py
@@ -37,4 +37,25 @@ class NoteAPIViewTests(APITestCase):
             minimal_data.append({'title': note['title'], 'description': note['description']})
         self.assertEqual(minimal_data, list_notes)
 
+    def test_note_detail_put(self):
+        """Test if PUT request can update a note."""
+        new_data = {'title': 'New Title', 'description': 'New Description'}
+        response = self.client.put('/apidrf/notes/1/',data=new_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        note = Note.objects.get(pk=1)
+        self.assertEqual(note.title, new_data['title'])
+        self.assertEqual(note.description, new_data['description'])
+    
+    def test_note_detail_put_required(self):
+        """Test if PUT request fails if any required field is not sent in the data."""
+        new_data = {'title': 'New Title'}
+        response = self.client.put('/apidrf/notes/1/',data=new_data, format='json')
+        self.assertEqual(response.content, b'{"description":["This field is required."]}')
+
+    def test_note_detail_delete(self):
+        """Test api can accept delete request to remove a note."""
+        response = self.client.delete('/apidrf/notes/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Note.objects.count(), 0)
+
 
