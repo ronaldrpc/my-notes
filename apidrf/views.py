053ca31c46095f35591fa90afbc575d38358741e
Ronald Paternina <ronaldpaternina11@gmail.com>
2024-01-16 14:37:01 -0500

    feat(apidrf): use generic class based views

--- apidrf/views.py
+++ apidrf/views.py
@@ -1,36 +1,21 @@
 
-from rest_framework import generics, mixins
-from rest_framework.response import Response
+from rest_framework import generics
 
 from apidrf.serializers import NoteSerializer
 from notes.models import Note
 
 
-class NoteList(mixins.ListModelMixin, mixins.CreateModelMixin, 
-               generics.GenericAPIView):
+class NoteList(generics.ListCreateAPIView):
     """List all notes or create new ones."""
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 
-class NoteDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin, generics.GenericAPIView):
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
     """Retrieve, update or delete a note."""
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
