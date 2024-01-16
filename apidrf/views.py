f879d9edc70ba1a12c59601024151d2d617a3886
Ronald Paternina <ronaldpaternina11@gmail.com>
2024-01-16 14:19:19 -0500

    feat(apidrf): use mixins and generic api view

--- apidrf/views.py
+++ apidrf/views.py
@@ -1,52 +1,36 @@
-from django.http import Http404
 
-from rest_framework import status
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from apidrf.serializers import NoteSerializer
 from notes.models import Note
 
 
-class NoteList(APIView):
+class NoteList(mixins.ListModelMixin, mixins.CreateModelMixin, 
+               generics.GenericAPIView):
     """List all notes or create new ones."""
-    def get(self, request, format=None):
-        notes = Note.objects.all()
-        serializer = NoteSerializer(notes, many=True)
-        return Response(serializer.data)
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
 
-    def post(self, request, format=None):
-        serializer = NoteSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class NoteDetail(APIView):
+class NoteDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin, generics.GenericAPIView):
     """Retrieve, update or delete a note."""
-    def get_object(self, pk):
-        try:
-            return Note.objects.get(pk=pk)
-        except Note.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk, format=None):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(note)
-        return Response(serializer.data)
-    
-    def put(self, request, pk, format=None):
-        note = self.get_object(pk)
-        serializer = NoteSerializer(note, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk, format=None):
-        note = self.get_object(pk)
-        note.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
