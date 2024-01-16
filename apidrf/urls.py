ca0a7a1a339191d7daf798601476d7c1808b3513
Ronald Paternina <ronaldpaternina11@gmail.com>
2024-01-16 14:11:09 -0500

    feat(apidrf): implement APIView

--- apidrf/urls.py
+++ apidrf/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from apidrf import views
 
 
 urlpatterns = [
-    path('notes/', views.notes_list_create),
-    path('notes/<int:pk>/', views.notes_detail),
+    path('notes/', views.NoteList.as_view()),
+    path('notes/<int:pk>/', views.NoteDetail.as_view()),
 ]
 
-
+urlpatterns = format_suffix_patterns(urlpatterns)
--- apidrf/views.py
+++ apidrf/views.py
@@ -1,45 +1,52 @@
-from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from apidrf.serializers import NoteSerializer
 from notes.models import Note
 
 
-@api_view(['GET', 'POST'])
-def notes_list_create(request):
+class NoteList(APIView):
     """List all notes or create new ones."""
-    if request.method == 'GET':
+    def get(self, request, format=None):
         notes = Note.objects.all()
         serializer = NoteSerializer(notes, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = NoteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def notes_detail(request, pk):
+
+class NoteDetail(APIView):
     """Retrieve, update or delete a note."""
-    note = get_object_or_404(Note, pk=pk)
+    def get_object(self, pk):
+        try:
+            return Note.objects.get(pk=pk)
+        except Note.DoesNotExist:
+            raise Http404
     
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        note = self.get_object(pk)
         serializer = NoteSerializer(note)
         return Response(serializer.data)
     
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        note = self.get_object(pk)
         serializer = NoteSerializer(note, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        note = self.get_object(pk)
         note.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
